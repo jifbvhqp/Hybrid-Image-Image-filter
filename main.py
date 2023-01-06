78266cb095e4e7c524f0e54af139a9014f8e42a9
品捷 黃 <jifbvhqp@gmail.com>
2023-01-06 13:13:35 +0800

    first

---
+++ main.py
@@ -0,0 +1,134 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from module import Dot,GassuianLowPassfilter,ButterworthLowPassfilter,idealLowPassfilter,GassuianHighPassfilter,ButterworthHighPassfilter,idealHighPassfilter,Inv_Fourier,Fourier,Transform,showFilter,show,salt_pepper_noise,gaussian_noise
+
+def concat_imgh(imgList):
+	if len(imgList) >= 2:
+		res = np.hstack((imgList[0],imgList[1]))
+		for i in range(2,len(imgList)):
+			res = np.hstack((res,imgList[i]))
+		return res
+	elif len(imgList) == 1:
+		return imgList[0]
+	else:
+		return []
+def concat_imgv(imgList):
+	if len(imgList) >= 2:
+		res = np.vstack((imgList[0],imgList[1]))
+		for i in range(2,len(imgList)):
+			res = np.vstack((res,imgList[i]))
+		return res
+	elif len(imgList) == 1:
+		return imgList[0]
+	else:
+		return []
+def LowPassFilter(img,D_0,tp = 'Gassiuan',n = 2):	
+	transimg = Transform(img)
+	FFt_img = Fourier(transimg)
+	
+	if tp == 'ideal':
+		lowPassFilter = idealLowPassfilter(img,D_0)
+	elif tp == 'Butterworth':
+		lowPassFilter = ButterworthLowPassfilter(img,D_0,n = n) 
+	else:
+		lowPassFilter = GassuianLowPassfilter(img,D_0)
+		
+	res = Dot(FFt_img,lowPassFilter)
+	res = Inv_Fourier(res)
+	res = np.real(res)
+	res = Transform(res)
+	return res
+
+def HighPassFilter(img,D_0,tp = 'Gassiuan',n = 2):	
+	transimg = Transform(img)
+	FFt_img = Fourier(transimg)
+	
+	if tp == 'ideal':
+		HighPassFilter = idealHighPassfilter(img,D_0)
+	elif tp == 'Butterworth':
+		HighPassFilter = ButterworthHighPassfilter(img,D_0,n = n)
+	else:
+		HighPassFilter = GassuianHighPassfilter(img,D_0)
+		
+	res = Dot(FFt_img,HighPassFilter)
+	res = Inv_Fourier(res)
+	res = np.real(res)
+	res = Transform(res)
+	return res+img,res
+
+def Hybrid(img1,img2,kernel_type,D_0_value,idx,sigma1 = 25,sigma2 = 10):
+	HighresImg = []
+	HighresEdge = []
+	LowresImg = []
+	
+	for tp in kernel_type:
+		HighresimgList = []
+		EdgeimgList = []
+		LowresimgList = []
+		
+		for D_0 in D_0_value:
+			highPassres,Edge = HighPassFilter(img1,D_0,tp = tp)
+			lowPassres = LowPassFilter(img2,D_0,tp = tp)
+			HighresimgList.append(highPassres)
+			EdgeimgList.append(Edge)
+			LowresimgList.append(lowPassres)
+
+		HighresImg.append(concat_imgh(HighresimgList))
+		HighresEdge.append(concat_imgh(EdgeimgList))
+		LowresImg.append(concat_imgh(LowresimgList))
+		
+	highPassres = concat_imgv(HighresImg)
+	Edges = concat_imgv(HighresEdge)
+	lowPassres = concat_imgv(LowresImg)
+	#show(highPassres)
+	#show(lowPassres)
+	#show(lowPassres+Edges)
+	
+	cv2.imwrite('result/highres/'+str(idx)+'.jpg', highPassres) 
+	cv2.imwrite('result/lowres/'+str(idx)+'.jpg', lowPassres)
+	cv2.imwrite('result/hybrid_1/'+str(idx)+'.jpg', lowPassres+Edges) 
+	
+	HybridImg = []
+	for tp in kernel_type:
+		highPassres,Edge = HighPassFilter(img1,sigma1,tp = tp)
+		lowPassres = LowPassFilter(img2,sigma2,tp = tp)
+		HybridImg.append(Edge+lowPassres)
+
+	hybridres = concat_imgh(HybridImg)
+	return hybridres
+
+if __name__ == '__main__':
+	img = cv2.imread('einstein.bmp', cv2.IMREAD_COLOR)
+	img = cv2.resize(img,(225,265))
+	row,col,channel = img.shape
+	#show(gaussian_noise(img,0,15))
+	#show(salt_pepper_noise(img, 0.1, 0.5))
+	
+	kernel_type = ['ideal','Butterworth','Gassiuan']
+	D_0_value = [10,50,100]
+	
+	imgL1 = ['Afghan_girl_after.jpg','1_bicycle.bmp','2_bird.bmp','3_cat.bmp','5_fish.bmp','6_makeup_before.jpg','a_1.png','b_1.png','einstein.bmp']
+	imgL2 = ['Afghan_girl_before.jpg','1_motorcycle.bmp','2_plane.bmp','3_dog.bmp','5_submarine.bmp','6_makeup_after.jpg','a_2.png','b_2.png','marilyn.bmp']
+
+	hybrids = []
+	idx,idx2 = 0,0
+
+	for im1,im2 in zip(imgL1,imgL2):
+		img1 = cv2.imread(im1, cv2.IMREAD_COLOR)
+		img1 = cv2.resize(img1,(225,265))
+		
+		img2 = cv2.imread(im2, cv2.IMREAD_COLOR)
+		img2 = cv2.resize(img2,(225,265))
+		hybrid = Hybrid(img1,img2,kernel_type,D_0_value,idx,sigma1 = 25,sigma2 = 10)
+		hybrids.append(hybrid)
+		idx += 1
+		
+		if idx % 3 == 0:
+			hybridsres = concat_imgv(hybrids)
+			cv2.imwrite('result/hybrid_2/'+str(idx2)+'.jpg', hybridsres) 
+			hybrids = []
+			idx2 += 1
+			
+	print('Done.')
+	#show(hybridsres)
---
+++ module.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+def gaussian_noise(image, mean = 0, sigma = 1):
+	img = np.copy(image)
+	noise = np.random.normal(mean, sigma, img.shape)
+	return np.clip(img + noise, 0, 255).astype('uint8')
+
+def salt_pepper_noise(image, fraction, salt_vs_pepper):
+	img = np.copy(image)
+	size = img.size
+	num_salt = np.ceil(fraction * size * salt_vs_pepper).astype('int')
+	num_pepper = np.ceil(fraction * size * (1 - salt_vs_pepper)).astype('int')
+	row, column,_ = img.shape
+
+	x = np.random.randint(0, column - 1, num_pepper)
+	y = np.random.randint(0, row - 1, num_pepper)
+	img[y, x] = 0  
+
+	x = np.random.randint(0, column - 1, num_salt)
+	y = np.random.randint(0, row - 1, num_salt)
+	img[y, x] = 255 
+	return img
+	
+def show(img):
+	cv2.imshow('My Image',img/255)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+def showFilter(mask):
+	cv2.imshow('My Image',mask)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+def Transform(img):
+	img = img.astype(int)
+	p,q,r = img.shape
+	for i in range(p):
+		for j in range(q):
+			if((i+j)%2)==1:
+				img[i,j,:] *= -1
+	return img
+	
+def Fourier(img):
+	FFt_img = np.zeros(img.shape,dtype=np.complex)
+	for i in range(img.shape[2]):
+		FFt_img[:,:,i] = np.fft.fft2(img[:,:,i])
+	return FFt_img
+
+def Inv_Fourier(img):
+	iFFt_img = np.zeros(img.shape,dtype=np.complex)
+	for i in range(img.shape[2]):
+		iFFt_img[:,:,i] = np.fft.ifft2(img[:,:,i])
+	return iFFt_img
+
+def idealHighPassfilter(img,D_0):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			if (x + y)**0.5 <= D_0:
+				res[i,j] = 0
+			else:
+				res[i,j] = 1 
+	return res
+
+def ButterworthHighPassfilter(img,D_0,n = 2):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			D_uv = (x+y)**0.5
+			res[i,j] = 1 - 1/(1 + ((D_uv/D_0)**(n*2)))
+	return res
+
+def GassuianHighPassfilter(img,D_0):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			res[i,j] = 1 - np.exp(-(x+y)/(2*D_0*D_0))
+	return res
+
+def idealLowPassfilter(img,D_0):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			if (x + y)**0.5 <= D_0:
+				res[i,j] = 1
+			else:
+				res[i,j] = 0 
+	return res
+	
+def ButterworthLowPassfilter(img,D_0,n = 2):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			D_uv = (x+y)**0.5
+			res[i,j] = 1/(1 + ((D_uv/D_0)**(n*2)))
+	return res	
+		
+def GassuianLowPassfilter(img,D_0):
+	row,col = img.shape[0],img.shape[1]
+	center_x,center_y = int(row/2),int(col/2)
+	res = np.zeros((row,col))
+	for i in range(row):
+		for j in range(col):
+			x = (i-center_x)**2
+			y = (j-center_y)**2
+			res[i,j] = np.exp(-(x+y)/(2*D_0*D_0))
+	return res
+
+def Dot(img,kernel):
+	res = np.zeros(img.shape,dtype=np.complex)
+	for i in range(img.shape[2]):
+		res[:,:,i] = img[:,:,i]*kernel
+	return res
